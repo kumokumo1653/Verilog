945c2a0eb28c366c0ec05d73c160fdb0f8f99e26
kumokumo1653 <fjsda526hdbs@gmail.com>
2021-07-21 00:08:26 +0900

    fix error i40001

--- i4001/Main.v
+++ i4001/Main.v
@@ -30,20 +30,34 @@ module Main(CLK, in, MCLK, SWITCH, RESET, seg_pattern, seg_digit, DMD_CLR, dmd_s
     reg [1:0]mode = 2'b00;//0...input 1...run 2...debug
     reg [10:0]column_id = 11'b00000000000;
     reg [3:0] read_id = 4'b0000;
-    reg [15:0] rom_out = 16'b0000_0000_0000_0000;
+    reg [7:0] input_page = 8'b0000_0000;
+    reg [7:0] debug_page = 8'b0000_0000;
+    wire [15:0] rom_out = 16'b0000_0000_0000_0000;
+
+    reg [3:0]seg_d1 = 4'b0000;
+    reg [3:0]seg_d2 = 4'b0000;
+    reg [3:0]seg_d3 = 4'b0000;
+    reg [3:0]seg_d4 = 4'b0000;
+
+    reg [15:0]dmd_in = 16'b0000_0000_0000_0000;
+    reg [4:0]dmd_in_column = 5'b00000;
+    reg ENTER = 1;
     
     always @ (SWITCH or in[15])begin
         //mode input
         if(SWITCH == 0)begin
             mode <= 0;
+            ENTER <= 1;
         end
         //mode run
         else if(in[15] == 0) begin
             mode <= 1;
+            ENTER <= 0;
         end
         //mode ddebug
         else begin
             mode <= 2;
+            ENTER <= 0;
         end
     end
 
@@ -53,14 +67,56 @@ module Main(CLK, in, MCLK, SWITCH, RESET, seg_pattern, seg_digit, DMD_CLR, dmd_s
         if(mode == 0)begin
             if(column_id == 11'b11111111111)begin
                 column_id <= 11'b00000000000;
+                input_page <= 8'b0000_0000;
             end else begin
                 column_id <= column_id + 11'b00000000001;
+                if(column_id % 32 == 0)begin
+                    input_page <= input_page + 8'b0000_0001;
+                end
+                
             end
+
+
+            seg_d1 <= input_page[3:0];
+            seg_d2 <= input_page[7:4];
+            seg_d3 <= 4'b0000;
+            seg_d4 <= 4'b0000;
+
+            dmd_in <= in;
+            dmd_in_column <= column_id[4:0];
             
+        end else if(mode == 1 || mode == 2)begin
+            //mode debug
+            if(in[12] == 0)begin
+                //i4001
+                if(in[13] == 1)begin
+                    //pageUp
+                    if(debug_page == 63)begin
+                        debug_page <= 8'b0000_0000;
+                    end else begin
+                        debug_page <= debug_page + 8'b0000_0001;
+                    end
+                end else if(in[13] == 0)begin
+                    //pageDown
+                    if(debug_page == 0)begin
+                        debug_page <= 8'b0011_1111;
+                    end else begin
+                        debug_page <= debug_page - 8'b0000_0001;
+                    end
+                end
+
+                seg_d1 <= debug_page[3:0];
+                seg_d2 <= debug_page[7:4];
+                seg_d3 <= 4'b0000;
+                seg_d4 <= 4'b0000;
+
+            end
         end
     end
 
+    //rom ....clk, read_id
+    //matrix column_id, load, in_column
     Rom rom(.CLK(MCLK), .column_id(column_id), .read_id(read_id), .in(in), .out(rom_out), .mode(mode));
-    Segment7 seg7(.CLK(CLK), .IN_CLR, .D1, .D2, .D3, .D4, .PATTERN(seg_pattern), .DIGIT(seg_digit));
-    Matrix matrix(.column_id, .in_column, .CLK(CLK), .IN_CLR, .LOAD, .RESET, .column_seg(dmd_seg), .out_column(dmd_column), .COLUMN_CLK(DMD_CLK), .OUT_CLR(DMD_CLR));
+    Segment7 seg7(.CLK(CLK), .IN_CLR(1'b1), .D1(seg_d1), .D2(seg_d2), .D3(seg_d3), .D4(seg_d4), .PATTERN(seg_pattern), .DIGIT(seg_digit));
+    Matrix matrix(.column_id(dmd_in_column), .in_column(dmd_in), .CLK(CLK), .IN_CLR(1'b1), .LOAD(MCLK & ENTER), .RESET(1'b0), .column_seg(dmd_seg), .out_column(dmd_column), .COLUMN_CLK(DMD_CLK), .OUT_CLR(DMD_CLR));
 endmodule
--- i4001/Rom.v
+++ i4001/Rom.v
@@ -27,7 +27,7 @@ module Rom(CLK, column_id, read_id, in, out, mode);
     output [15:0]out;
     
     reg [15:0]rom_out[0:15];
-    reg [15:0]we = 16'b0000_0000_0000_0000;
+    wire [15:0]we;
 
     initial begin
         rom_out[0] = 16'b0000_0000_0000_0000;
@@ -52,22 +52,22 @@ module Rom(CLK, column_id, read_id, in, out, mode);
     assign out = rom_out[read_id];
     SelectorRom selector(.selector(column_id[10:7]), .mode(mode), .out(we));
 
-    i40001 r0(.CLK(CLK), .WE(we[0]), .address(column_id[6:0]), .in(in), .out(rom_out[0]));
-    i40001 r1(.CLK(CLK), .WE(we[1]), .address(column_id[6:0]), .in(in), .out(rom_out[1]));
-    i40001 r2(.CLK(CLK), .WE(we[2]), .address(column_id[6:0]), .in(in), .out(rom_out[2]));
-    i40001 r3(.CLK(CLK), .WE(we[3]), .address(column_id[6:0]), .in(in), .out(rom_out[3]));
-    i40001 r4(.CLK(CLK), .WE(we[4]), .address(column_id[6:0]), .in(in), .out(rom_out[4]));
-    i40001 r5(.CLK(CLK), .WE(we[5]), .address(column_id[6:0]), .in(in), .out(rom_out[5]));
-    i40001 r6(.CLK(CLK), .WE(we[6]), .address(column_id[6:0]), .in(in), .out(rom_out[6]));
-    i40001 r7(.CLK(CLK), .WE(we[7]), .address(column_id[6:0]), .in(in), .out(rom_out[7]));
-    i40001 r8(.CLK(CLK), .WE(we[8]), .address(column_id[6:0]), .in(in), .out(rom_out[8]));
-    i40001 r9(.CLK(CLK), .WE(we[9]), .address(column_id[6:0]), .in(in), .out(rom_out[9]));
-    i40001 r10(.CLK(CLK), .WE(we[10]), .address(column_id[6:0]), .in(in), .out(rom_out[10]));
-    i40001 r11(.CLK(CLK), .WE(we[11]), .address(column_id[6:0]), .in(in), .out(rom_out[11]));
-    i40001 r12(.CLK(CLK), .WE(we[12]), .address(column_id[6:0]), .in(in), .out(rom_out[12]));
-    i40001 r13(.CLK(CLK), .WE(we[13]), .address(column_id[6:0]), .in(in), .out(rom_out[13]));
-    i40001 r14(.CLK(CLK), .WE(we[14]), .address(column_id[6:0]), .in(in), .out(rom_out[14]));
-    i40001 r15(.CLK(CLK), .WE(we[15]), .address(column_id[6:0]), .in(in), .out(rom_out[15]));
+    i4001 r0(.CLK(CLK), .WE(we[0]), .address(column_id[6:0]), .in(in), .out(rom_out[0]));
+    i4001 r1(.CLK(CLK), .WE(we[1]), .address(column_id[6:0]), .in(in), .out(rom_out[1]));
+    i4001 r2(.CLK(CLK), .WE(we[2]), .address(column_id[6:0]), .in(in), .out(rom_out[2]));
+    i4001 r3(.CLK(CLK), .WE(we[3]), .address(column_id[6:0]), .in(in), .out(rom_out[3]));
+    i4001 r4(.CLK(CLK), .WE(we[4]), .address(column_id[6:0]), .in(in), .out(rom_out[4]));
+    i4001 r5(.CLK(CLK), .WE(we[5]), .address(column_id[6:0]), .in(in), .out(rom_out[5]));
+    i4001 r6(.CLK(CLK), .WE(we[6]), .address(column_id[6:0]), .in(in), .out(rom_out[6]));
+    i4001 r7(.CLK(CLK), .WE(we[7]), .address(column_id[6:0]), .in(in), .out(rom_out[7]));
+    i4001 r8(.CLK(CLK), .WE(we[8]), .address(column_id[6:0]), .in(in), .out(rom_out[8]));
+    i4001 r9(.CLK(CLK), .WE(we[9]), .address(column_id[6:0]), .in(in), .out(rom_out[9]));
+    i4001 r10(.CLK(CLK), .WE(we[10]), .address(column_id[6:0]), .in(in), .out(rom_out[10]));
+    i4001 r11(.CLK(CLK), .WE(we[11]), .address(column_id[6:0]), .in(in), .out(rom_out[11]));
+    i4001 r12(.CLK(CLK), .WE(we[12]), .address(column_id[6:0]), .in(in), .out(rom_out[12]));
+    i4001 r13(.CLK(CLK), .WE(we[13]), .address(column_id[6:0]), .in(in), .out(rom_out[13]));
+    i4001 r14(.CLK(CLK), .WE(we[14]), .address(column_id[6:0]), .in(in), .out(rom_out[14]));
+    i4001 r15(.CLK(CLK), .WE(we[15]), .address(column_id[6:0]), .in(in), .out(rom_out[15]));
 
 	
 endmodule
